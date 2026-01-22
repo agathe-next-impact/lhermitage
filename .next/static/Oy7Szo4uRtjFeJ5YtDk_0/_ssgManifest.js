a00588f1d8ae1a26812b2f5ec66acc3568b3e806
agathe <agathe@agat.dev>
2026-01-22 10:45:55 +0100

    structures bug

--- .next/static/Oy7Szo4uRtjFeJ5YtDk_0/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[...slug]","\u002Factivite\u002F[slug]","\u002Fhebergement\u002F[slug]","\u002Fsejour\u002F[slug]","\u002Fstructure\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
